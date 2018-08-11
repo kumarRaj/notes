4d024334e3d756388973843e8e8054171d1d2004
sunandan <sunandan.riders@gmail.com>
2018-08-11 17:34:35 +0530

    Registration mapping

--- src/main/java/com/wander/controller/LoginController.java
+++ src/main/java/com/wander/controller/LoginController.java
@@ -2,7 +2,9 @@ package com.wander.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
 
 import com.wander.model.Registration;
 
@@ -11,9 +13,18 @@ public class LoginController {
 
 	
 	 @RequestMapping(value = "/registration")
-	    public String create(Model registrationForm){
-		 	registrationForm.addAttribute("registrationForm", new Registration());
+	    public String Registation(Model registrationForm){
+		 	Registration r = new Registration();
+		 	r.setFirstName("abcdef");
+		 	registrationForm.addAttribute("registrationForm", r);
 		 	return "registration.html";
 	    }
-
+	 @RequestMapping(value = "/registration-Form")
+	    public String registerUser(@ModelAttribute("registrationForm") Registration registrationForm){
+		 System.out.println("Raj Kumar"+registrationForm.getFirstName());
+		 System.out.println("Raj Kumar"+registrationForm.getLastName());	
+		 //registrationForm.addAttribute("registrationForm", new Registration());
+		 	return "home.html";
+	    }
 }
+
