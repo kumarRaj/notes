47b4ae6160f7f80c3c4d537bd8b843f9d5c062e5
Raj <rajkum@thoughtworks.com>
2018-08-13 21:18:09 +0530

    Changing to unix style line endings

--- src/main/java/com/wander/WanderApplication.java
+++ src/main/java/com/wander/WanderApplication.java
@@ -1,10 +1,7 @@
 package com.wander;
 
-import com.wander.controller.BadPerson;
-import com.wander.model.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WanderApplication {
